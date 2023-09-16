e0665133614be38f92abf0e91761ba6c558763fa
young310 <tiyang310@gmail.com>
2023-09-16 17:41:46 +0800

    Update app.py
    shorten prompt

--- app.py
+++ app.py
@@ -27,11 +27,6 @@ def index():
 
 def generate_prompt(animal):
     return """Suggest three names for an animal that is a superhero. 用中文
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
 Animal: {}
 Names:""".format(
         animal.capitalize()
