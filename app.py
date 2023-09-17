39a9e7201663dc518045aea697a428bbd73b94b3
young310 <tiyang310@gmail.com>
2023-09-17 18:01:53 +0800

    revise table

--- app.py
+++ app.py
@@ -33,6 +33,25 @@ Names:""".format(
         animal.capitalize()
     )
 
+@app.route("/acc", methods=("GET","POST"))
+def acc_index():
+    if request.method == "POST":
+        question = request.form["question"]
+        file_content = open(os.getcwd() + "\\transaction.txt").readlines()
+        response = response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "以下都用繁體中文跟html語法回答，越短越好"},
+                {"role": "system", "content": "你是一個具備十年經驗的會計師，專精審計與洗錢防制，我明天要開始當審計員了，可以幫我看一下資料?"},
+                {"role": "user", "content": "用html畫成表格回答，只要<table>到</table>的內容，其他不要"},
+                {"role": "user", "content": question + "------" + ' '.join(file_content)}
+                
+            ]
+        )
+        return redirect(url_for("acc_index", result=response.choices[0].message.content))
+    result = request.args.get("result")
+    return render_template("acc_index.html", result=result)
+
 @app.route("/tax", methods=("GET","POST"))
 def tax_index():
     if request.method == "POST":
@@ -46,26 +65,6 @@ def tax_index():
                 
             ]
         )
-        return redirect(url_for("tax_index", result=response.choices[0].message.content))
+        return redirect(url_for("tax_index", result=response.choices[0].message.content[response.choices[0].message.content.index('<table>'):response.choices[0].message.content.index('</table>')]))
     result = request.args.get("result")
     return render_template("tax_index.html", result=result)
-
-@app.route("/acc", methods=("GET", "POST"))
-def add_index():
-    if request.method == "POST":
-        question = request.form["question"]
-        file_content = open(os.getcwd() + "\\transaction.txt").readlines()
-        print(question + "------" + ' '.join(file_content))
-        response = response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "以下都用繁體中文回答，越短越好"},
-                {"role": "system", "content": "你是一個具備十年經驗的會計師，專精審計與洗錢防制，我明天要開始當審計員了，可以幫我看一下資料?"},
-                {"role": "user", "content": question + "------" + ' '.join(file_content)}
-                
-            ]
-        )
-        print(response.choices[0].message.content)
-        return redirect(url_for("acc_index", result=response.choices[0].message.content))
-    result = request.args.get("result")
-    return render_template("acc_index.html", result=result)
