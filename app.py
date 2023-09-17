897fd9fff3486ae2cae72f71de2de0d9e15e0fc2
young310 <tiyang310@gmail.com>
2023-09-17 21:49:09 +0800

    add accountant support

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from flask import Flask, redirect, render_template, request, url_for
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
+file_content = open(os.getcwd() + "\\journal1.txt").readlines()
+print(file_content)
 
 @app.route("/", methods=("GET", "POST"))
 def index():
@@ -37,13 +38,19 @@ Names:""".format(
 def acc_index():
     if request.method == "POST":
         question = request.form["question"]
-        file_content = open(os.getcwd() + "\\transaction.txt").readlines()
+        if '彙總' in question:
+            file_content = open(os.getcwd() + "\\journal1.txt").readlines()
+        else:
+            file_content = open(os.getcwd() + "\\transaction.txt").readlines()
+        
+        #請幫我彙總資料，把年度放在column，把費用類別放在row，內容放借項，根據你產生的表格幫我做異常的費用
+        #請幫我彙總資料，把年度放在column，把費用類別放在row，內容放借項，使用<table>語法畫成表格
         response = response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
-                {"role": "system", "content": "以下都用繁體中文跟html語法回答，越短越好"},
-                {"role": "system", "content": "你是一個具備十年經驗的會計師，專精審計與洗錢防制，我明天要開始當審計員了，可以幫我看一下資料?"},
-                {"role": "user", "content": "用html畫成表格回答，只要<table>到</table>的內容，其他不要"},
+                {"role": "system", "content": "以下都用繁體中文跟html語法回答"},
+                {"role": "system", "content": "你是一個具備十年經驗的會計師，專精審計與洗錢防制，我明天要開始當審計員了，可以幫我看一下資料並給一些建議?"},
+                {"role": "user", "content": "用html畫成表格回答"},
                 {"role": "user", "content": question + "------" + ' '.join(file_content)}
                 
             ]
@@ -65,6 +72,6 @@ def tax_index():
                 
             ]
         )
-        return redirect(url_for("tax_index", result=response.choices[0].message.content[response.choices[0].message.content.index('<table>'):response.choices[0].message.content.index('</table>')]))
+        return redirect(url_for("tax_index", result=response.choices[0].message.content))
     result = request.args.get("result")
     return render_template("tax_index.html", result=result)
